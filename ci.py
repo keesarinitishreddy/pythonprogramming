be64d7078f52e91bec09b471bbb8e1f8cf849353
keesarinitishreddy <keesarinitishreddy@gmail.com>
2025-03-21 16:24:54 +0530

    Delete ci.py

--- ci.py
+++
@@ -1,6 +0,0 @@
-p=int(input("enter the principle amount : "))
-t=int(input("enter time : "))
-r=int(input("enter rate : "))
-amount=p*(1+r/100)**t
-ci=amount-p
-print("The simple compound intrest is :",ci)
