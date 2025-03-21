6f1ca0a5166ffe32713dc13c96363c970fb16bec
keesarinitishreddy <keesarinitishreddy@gmail.com>
2025-03-21 16:14:07 +0530

    Create Details.py

---
+++ Details.py
@@ -0,0 +1,8 @@
+Name = input("Enter Your Name : ")
+Address = input("Enter Your Address : ")
+Gmail = input("Enter Your Gmail : ")
+MobileNumber = input("Enter Your MobileNumber : ")
+print("NAME : ",Name)
+print("ADDRESS : ", Address)
+print("GMAIL : ", Gmail)
+print("MOBILE NUMBER : ",MobileNumber)
