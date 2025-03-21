30cd5a1d4dd4510b8a85cf6466529d9609acc52c
keesarinitishreddy <keesarinitishreddy@gmail.com>
2025-03-21 16:21:48 +0530

    Create Distance.py

---
+++ Distance.py
@@ -0,0 +1,6 @@
+a1 =int(input("enter the value of a1 = "))
+a2 =int(input("enter the value of a2 = "))
+b1 =int(input("enter the value of b1 = "))
+b2 =int(input("enter the value of a2 = "))
+Distance = ((a2-a1)**2+(b2-b1)**2)**0.5
+print("Distance between two points is : ",round(Distance,2))
